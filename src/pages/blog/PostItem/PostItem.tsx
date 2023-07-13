30fefaca9250b4910de6b1167c77b03a96ac7db0
DoSon <sonnvc2001@gmail.com>
2023-07-13 15:20:16 +0700

    Lấy một state trong Redux bằng useSelector()

--- src/pages/blog/PostItem/PostItem.tsx
+++ src/pages/blog/PostItem/PostItem.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import { Post } from '~/types/blog.type'
 
-const PostItem = () => {
+interface PostItemType{
+  post:Post
+  key:string
+}
+const PostItem = ({post}:PostItemType) => {
   return (
     <div className='flex flex-col items-center overflow-hidden rounded-lg border md:flex-row'>
       <div className='group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48'>
         <img
-          src='https://img.thuthuatphanmem.vn/uploads/2018/10/09/anh-dep-nhat-the-gioi-ve-thien-nhien_041753462.jpg'
+          src={post.featuredImage}
           loading='lazy'
           className='absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+          alt={post.title}
         />
       </div>
       <div className='flex flex-col gap-2 p-4 lg:p-6'>
-        <span className='text-sm text-gray-400'>test</span>
-        <h2 className='text-xl font-bold text-gray-800'>Title</h2>
-        <p className='text-gray-500'>post description</p>
+        <span className='text-sm text-gray-400'>{post.publishDate}</span>
+        <h2 className='text-xl font-bold text-gray-800'>{post.title}</h2>
+        <p className='text-gray-500'>{post.description}</p>
         <div>
           <div className='inline-flex rounded-md shadow-sm' role='group'>
             <button
--- src/pages/blog/PostList/PostList.tsx
+++ src/pages/blog/PostList/PostList.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '~/store'
 import PostItem from '../PostItem'
 
 const PostList = () => {
+  const postList = useSelector((state:RootState) => state.blog.postList)
+  console.log(postList);
+
   return (
     <div className='bg-white py-6 sm:py-8 lg:py-12'>
       <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
@@ -12,7 +17,9 @@ const PostList = () => {
           </p>
         </div>
         <div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8'>
-          <PostItem />
+          {
+            postList.length > 0 && postList.map((post)=><PostItem post={post} key={post.id} />) 
+          }
         </div>
       </div>
     </div>
