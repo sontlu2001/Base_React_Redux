3f8fa0a2f8c89c706bf6deedb856e62cefea11fe
DoSon <sonnvc2001@gmail.com>
2023-07-21 20:11:31 +0700

    Chia Route cơ bản

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MainLayout from './layouts/MainLayout'
+import Dashboard from './pages/Dashboard'
+import { Route, Routes } from 'react-router'
+import About from './pages/About'
+import StaffList from './pages/StaffList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <MainLayout>
+      <Routes>
+        <Route path='/' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/staff' element={<StaffList></StaffList>}></Route>
+      </Routes>
+    </MainLayout>
   )
 }
 
